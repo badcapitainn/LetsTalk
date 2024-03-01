5403c996f8738c9939f17cc2a3a71c8860ce77fb
Prince <madzambaprince111@gmail.com>
2024-03-01 11:56:14 +0200

    still fixing conversation function

--- app/src/main/java/com/example/letstalk/screens/ConversationScreen.kt
+++ app/src/main/java/com/example/letstalk/screens/ConversationScreen.kt
@@ -2,7 +2,11 @@ package com.example.letstalk.screens
 
 import android.Manifest
 import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
@@ -11,7 +15,10 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.letstalk.contract.RecognitionContract
@@ -20,11 +27,14 @@ import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
 
+
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun ConversationScreen(
     viewModel: MainViewModel = viewModel()
 ){
+    val context = LocalContext.current
+    val state = viewModel.state.value
     val permissionState = rememberPermissionState(
         permission = Manifest.permission.RECORD_AUDIO
     )
@@ -42,20 +52,44 @@ fun ConversationScreen(
         modifier = Modifier
             .fillMaxSize()
     ) {
-        IconButton(onClick = {
-            if (permissionState.status.isGranted){
-                recognitionLauncher.launch(Unit)
-            }else{
-                permissionState.launchPermissionRequest()
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            IconButton(onClick = {
+                if (permissionState.status.isGranted){
+                    recognitionLauncher.launch(Unit)
+                }else{
+                    permissionState.launchPermissionRequest()
+                }
+            }) {
+                Icon(Icons.Default.Mic, contentDescription = "Record")
             }
-        }) {
-            Icon(Icons.Default.Mic, contentDescription = "Record")
+
+            if (viewModel.state.value.recordedText != null){
+                //to remove the square brackets
+                val originalString = viewModel.state.value.recordedText!!
+                val formattedString = originalString.replace(Regex("^\\[(.+)\\]$"), "$1")
+                Text(
+                    text = formattedString,
+                    fontSize = 24.sp
+                )
+                viewModel.onButtonClick(
+                    text = formattedString,
+                    context = context
+                )
+
+                Spacer(modifier = Modifier.height(15.dp))
+                Text(text = state.translatedText)
+
+            }
+
+
+
         }
+
     }
-    if (viewModel.state.value.recordedText != null){
-        androidx.compose.material3.Text(
-            text = viewModel.state.value.recordedText!!,
-            fontSize = 24.sp
-        )
-    }
+
 }
